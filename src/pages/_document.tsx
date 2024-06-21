d95549e1bd0ff869d18fda56deee02968f7a1728
songa <thddk0121@gmail.com>
2024-06-21 23:59:57 +0900

    feat #8 : Document 컴포넌트 HTML 구조 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return <Html lang="ko"></Html>;
+  return (
+    <Html lang="ko">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
