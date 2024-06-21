5264cb0c1bf84f3c32ab970ffaecd373765dd049
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-06-22 00:14:14 +0900

    feat #5 : 로그인 페이지 제작

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,23 @@
-export default function first() {
-  <div>hsdffsfsfi</div>;
+import Image from 'next/image';
+import Link from 'next/link';
+
+import logo from '@/../public/assets/images/biglogo.png';
+import LoginForm from '@/form/login/LoginForm';
+
+import styles from './LoginPage.module.scss';
+
+export default function LoginPage() {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.formContainer}>
+        <div className={styles.logoContainer}>
+          <Image src={logo} alt="로고" width={248} height={45} />
+        </div>
+        <LoginForm />
+        <Link className={styles.signupLink} href="/signup">
+          아직 회원이 아니신가요? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
