c506ba537da49f370f0b8870d295ae70aefdedfc
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-06-22 15:02:51 +0900

    feat #11 : contextAPI 로 로그인 상태 지정

--- src/form/login/LoginForm.tsx
+++ src/form/login/LoginForm.tsx
@@ -1,9 +1,11 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import Input from '@/components/auth/InputComponents';
+import { useAuth } from '@/contexts/AuthProvider';
 import { LoginUser } from '@/pages/api/LoginUser';
 import { loginSchema } from '@/utils/validation/Schema';
 
@@ -13,9 +15,11 @@ interface LoginFormValues {
   email: string;
   password: string;
 }
+
 export default function LoginForm() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
+  const { setUser, setToken } = useAuth();
 
   const {
     handleSubmit,
@@ -31,15 +35,23 @@ export default function LoginForm() {
       const { email, password } = data;
       const requestData = { email, password };
       const response = await LoginUser(requestData);
-      if (response) {
+      if (response && response.item && response.item.token) {
         console.log('로그인 성공');
-        router.prefetch('/');
+        // 토큰을 쿠키와 Context에 저장
+        Cookies.set('token', response.item.token, { expires: 7 });
+        setToken(response.item.token);
+        // 사용자 정보를 Context에 저장
+        setUser(response.item.user.item);
+        router.push('/'); // 로그인 성공 후 홈페이지로 리다이렉트
+      } else {
+        setError('로그인에 실패했습니다. 토큰을 받지 못했습니다.');
       }
     } catch (error) {
       console.error('로그인 실패:', error);
       setError('로그인에 실패했습니다. 다시 시도해주세요.');
     }
   });
+
   return (
     <>
       <form className={styles.form} onSubmit={handleLogin}>
@@ -67,6 +79,7 @@ export default function LoginForm() {
           로그인
         </button>
       </form>
+      {error && <p className={styles.errorMessage}>{error}</p>}
     </>
   );
 }
