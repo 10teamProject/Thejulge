acba1edcc174ccb33cbeb46b5ba54598b0355da5
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2024-06-24 19:43:35 +0900

    Feat/#24 notice list (#28)
    * feat #24: 공고카드 api 연결
    
    * design #24: 시급 증가율 한줄로
    
    * design #24: 맞춤공고 가로 정렬

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bootcamp-project-api.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/6-10/the-julge/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bootcamp-project-api.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/0-1/the-julge/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
