ee0c9310b933beb18d5954ec95312243a2c2ce86
yongin <rladls6412@naver.com>
2024-06-24 19:39:49 +0900

    가게정보 API 연결

--- src/pages/detailPage/index.tsx
+++ src/pages/detailPage/index.tsx
@@ -5,21 +5,68 @@ import location from '@/public/assets/images/location.png';
 import time from '@/public/assets/images/timers.png';
 import styles from './DetailPage.module.scss';
 import Card from '@/components/detailPage/Card';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { instance } from '../api/AxiosInstance';
 
-function DetailPage() {
+interface Props {
+  shopid: string;
+  noticeid: string;
+}
+
+interface StoreData {
+  item: {
+    closed?: boolean;
+    hourlyPay: number;
+    description: string;
+    startsAt: string;
+    workhour: number;
+    shop: {
+      item: {
+        category: string;
+        name: string;
+        imageUrl: string;
+
+        address1: string;
+        address2?: string;
+        description: string;
+      };
+    };
+  };
+}
+
+function DetailPage({ shopid, noticeid }: Props) {
+  const [storeData, setStoreData] = useState<StoreData | null>(null);
   const [isApplied, setIsApplied] = useState(false);
   const applyButton = () => {
     setIsApplied(!isApplied);
   };
+  console.log(storeData);
+
+  async function getData() {
+    // const res = await instance.get(`/shops/${shopid}/notices/${noticeid}`);
+    const res = await instance.get(
+      // 샘플 데이터 주소
+      `/shops/63fcc375-5d0a-4ba4-ac5b-101b03973c74/notices/2ad3ac93-0054-442e-a882-d6cce8c10470`,
+    );
+    const nextRes = res.data;
+    setStoreData(nextRes);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (storeData === null) {
+    return; // 처음에 null이 있으면 화면에 렌더링 오류가 발생할 수 있으니 if문을 줘서 오류발생을 막음
+  }
 
   return (
     <>
       <div className={styles.datail_container}>
         <div className={styles.shop_box}>
           <div className={styles.shop_title}>
-            <h1>한식</h1>
-            <h2>도토리식당</h2>
+            <h1>{storeData.item.shop.item.category}</h1>
+            <h2>{storeData.item.shop.item.name}</h2>
           </div>
 
           <div className={styles.shop_info}>
@@ -29,23 +76,20 @@ function DetailPage() {
             <div className={styles.shop_contents}>
               <h1>시급</h1>
               <div className={styles.shop_hourlPay}>
-                15000원
+                {storeData.item.hourlyPay}
                 <span>
                   기존 시급보다 50% <Image src={arrow} alt="상승" />
                 </span>
               </div>
               <div className={styles.startsAt}>
                 <Image src={time} alt="근무일" />
-                2024-06-21 15:00~18:00
+                {storeData.item.startsAt} ({storeData.item.workhour}시간)
               </div>
               <div className={styles.address}>
                 <Image src={location} alt="위치" />
-                부산 북구
+                {storeData.item.shop.item.address1}
               </div>
-              <p>
-                알바하기 편한 너구리네 라면집!라면 올려두고 끓이기만 하면 되어서
-                쉬운 편에 속하는 가게입니다
-              </p>
+              <p>{storeData.item.shop.item.description}</p>
               <div>
                 <button
                   className={`${styles.button} ${isApplied ? styles.true : styles.false}`}
@@ -59,11 +103,7 @@ function DetailPage() {
 
           <div className={styles.notice_description}>
             <h1>공고설명</h1>
-            <p>
-              기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가
-              비네요. 급해서 시급도 높였고 그렇게 바쁜 날이 아니라서
-              괜찮을거예요
-            </p>
+            <p>{storeData.item.description}</p>
           </div>
         </div>
       </div>
