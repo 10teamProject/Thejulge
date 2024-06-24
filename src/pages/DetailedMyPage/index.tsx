52c0202470223c4311331d858f2c59fcf76a88bc
Choi <soyeongvely@gmail.com>
2024-06-24 18:17:33 +0900

    feat #27 : 프로필 상세페이지에서 등록하기 페이지로 이동

--- src/pages/DetailedMyPage/index.tsx
+++ src/pages/DetailedMyPage/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './DetailedMyPage.module.scss';
-
+import { useRouter } from 'next/router';
 
 const DetailedMyPage = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/PostMyPage');
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.mainContainer}>
         <h1 className={styles.profileTitle}>내 프로필</h1>
         <div className={styles.profileBox}>
           <p className={styles.profileContent}>내 프로필을 등록하고 원하는 가게에 지원해 보세요.</p>
-          <button className={styles.profileButton}><span>내 프로필 등록하기</span></button>
+          <button onClick={handleClick} className={styles.profileButton}><span>내 프로필 등록하기</span></button>
         </div>
       </div>
     </main>
---
+++ src/pages/PostMyPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PostMyPage = () => {
+
+  return (
+    <>
+    <h1>안녕</h1>
+    </>
+  );
+ };
+
+export default PostMyPage;
