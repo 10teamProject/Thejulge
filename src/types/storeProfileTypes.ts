39cfd12ccac3a312af80f72df46a3a03fdd6bd0c
songa <thddk0121@gmail.com>
2024-06-25 21:19:28 +0900

    fix #18 : 가게 정보 등록 api 연동 작업중

---
+++ src/types/storeProfileTypes.ts
@@ -0,0 +1,9 @@
+export type storeProfileProps = {
+  name: string;
+  category: string;
+  address1: string;
+  address2: string;
+  description: string;
+  imageUrl: string;
+  originalHourlyPay: number;
+};
