2e558dde2f5d2c962b9246b60bc9feb19c515835
Choi <soyeongvely@gmail.com>
2024-06-25 18:17:30 +0900

    desigin #27 : 등록 input 및 지역 데이터 배열

--- src/pages/PostMyPage/index.tsx
+++ src/pages/PostMyPage/index.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
+import styles from './PostMyPage.module.scss';
 
+interface PostMyPageProps {}
 
-const PostMyPage = () => {
+const addressValue = [
+  { value: 'Jongno-gu', label: '서울시 종로구' },
+  { value: 'Jung-gu', label: '서울시 중구' },
+  { value: 'Yongsan-gu', label: '서울시 용산구' },
+  { value: 'Seongdong-gu', label: '서울시 성동구' },
+  { value: 'Gwangjin-gu', label: '서울시 광진구' },
+  { value: 'Dongdaemun-gu', label: '서울시 동대문구' },
+  { value: 'Jungnang-gu', label: '서울시 중랑구' },
+  { value: 'Seongbuk-gu', label: '서울시 성북구' },
+  { value: 'Gangbuk-gu', label: '서울시 강북구' },
+  { value: 'Dobong-gu', label: '서울시 도봉구' },
+  { value: 'Nowon-gu', label: '서울시 노원구' },
+  { value: 'Eunpyeong-gu', label: '서울시 은평구' },
+  { value: 'Seodaemun-gu', label: '서울시 서대문구' },
+  { value: 'Mapo-gu', label: '서울시 마포구' },
+  { value: 'Yangcheon-gu', label: '서울시 양천구' },
+  { value: 'Gangseo-gu', label: '서울시 강서구' },
+  { value: 'Guro-gu', label: '서울시 구로구' },
+  { value: 'Geumcheon-gu', label: '서울시 금천구' },
+  { value: 'Yeongdeungpo-gu', label: '서울시 영등포구' },
+  { value: 'Dongjak-gu', label: '서울시 동작구' },
+  { value: 'Gwanak-gu', label: '서울시 관악구' },
+  { value: 'Seocho-gu', label: '서울시 서초구' },
+  { value: 'Gangnam-gu', label: '서울시 강남구' },
+  { value: 'Songpa-gu', label: '서울시 송파구' },
+  { value: 'Gangdong-gu', label: '서울시 강동구' },
+];
 
+const PostMyPage: React.FC<PostMyPageProps> = () => {
   return (
-    <main>
-      <h1>내 프로필</h1>
+    <main className={styles.main}>
+      <div>
+        <h1 className={styles.profileTitle}>내 프로필</h1>
+        <form>
+          <label htmlFor="name">이름</label>
+          <input type="text" id="name" name="name" placeholder="입력" />
+          
+          <label htmlFor="tel">연락처</label>
+          <input type="tel" id="tel" name="tel" placeholder="입력" />
+          
+          <label htmlFor="region">선호 지역</label>
+          <select name="region" id="region">
+            {addressValue.map(option => (
+              <option key={option.value} value={option.value}>{option.label}</option>
+            ))}
+          </select>
+          
+          <label htmlFor="intro">소개</label>
+          <input type="text" id="intro" name="intro" placeholder="입력" />
+        </form>
+      </div>
     </main>
   );
- };
+};
 
 export default PostMyPage;
