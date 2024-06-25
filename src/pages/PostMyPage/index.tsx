f242bcda534d1da8e4ca37808a2d42219026e03e
Choi <soyeongvely@gmail.com>
2024-06-26 02:14:54 +0900

    design #27 : 내 프로필 등록 UI 미완성

--- src/pages/PostMyPage/index.tsx
+++ src/pages/PostMyPage/index.tsx
@@ -34,24 +34,38 @@ const addressValue = [
 const PostMyPage: React.FC<PostMyPageProps> = () => {
   return (
     <main className={styles.main}>
-      <div>
+      <div className={styles.postContainer}>
         <h1 className={styles.profileTitle}>내 프로필</h1>
-        <form>
-          <label htmlFor="name">이름</label>
-          <input type="text" id="name" name="name" placeholder="입력" />
-          
-          <label htmlFor="tel">연락처</label>
-          <input type="tel" id="tel" name="tel" placeholder="입력" />
-          
-          <label htmlFor="region">선호 지역</label>
-          <select name="region" id="region">
-            {addressValue.map(option => (
-              <option key={option.value} value={option.value}>{option.label}</option>
-            ))}
-          </select>
-          
-          <label htmlFor="intro">소개</label>
-          <input type="text" id="intro" name="intro" placeholder="입력" />
+        <form className={styles.formInput}>
+            <div className={styles.formarray}>
+              <div className={styles.inputSize}>
+                <label htmlFor="name" className={styles.inputFont}>이름</label>
+                <div><input className={styles.input} type="text" id="name" name="name" placeholder="입력" /></div>
+              </div>
+            
+              <div className={styles.inputSize}>
+                <label htmlFor="tel" className={styles.inputFont}>연락처</label>
+                <div><input className={styles.input} type="tel" id="tel" name="tel" placeholder="입력" /></div>
+              </div>
+
+              <div className={styles.inputSize}>
+                <label htmlFor="region" className={styles.inputFont}>선호 지역</label>
+                <div>
+                  <select className={styles.input} name="region" id="region">
+                  {addressValue.map(option => (
+                    <option key={option.value} value={option.value}>{option.label}</option>
+                  ))}
+                  </select>
+                </div>
+              </div> 
+            </div>
+            
+            <div className={styles.introduceBox}>
+              <label htmlFor="intro" className={styles.inputFont}>소개</label>
+              <div><input className={styles.inputBoard} type="text" id="intro" name="intro" placeholder="입력" /></div>
+            </div>
+
+              <button className={styles.button}><span>등록하기</span></button>
         </form>
       </div>
     </main>
