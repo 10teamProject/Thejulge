5eadc3f79525934203f079c2336d5ebee5ffb040
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-06-26 21:38:16 +0900

    feat #30 : 내 가게 정보 조회 제작

---
+++ src/components/myStore/MyStore.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+
+import { StoreInfo } from '@/pages/api/GetMystore';
+import locationIcon from '@/public/assets/icon/location.svg';
+
+import styles from './MyStore.module.scss';
+
+interface StoreCardProps {
+  storeData: StoreInfo;
+}
+
+const StoreCard: React.FC<StoreCardProps> = ({ storeData }) => (
+  <div className={styles.card}>
+    <div className={styles.imageContainer}>
+      {storeData.imageUrl && (
+        <img
+          src={storeData.imageUrl}
+          alt={storeData.name}
+          className={styles.image}
+        />
+      )}
+    </div>
+    <div className={styles.content}>
+      <span className={styles.category}>식당</span>
+      <h2 className={styles.storeName}>{storeData.name}</h2>
+      <p className={styles.location}>
+        <Image src={locationIcon} alt="Location" className={styles.icon} />
+        {storeData.address1} {storeData.address2}
+      </p>
+      <p className={styles.description}>{storeData.description}</p>
+      <div className={styles.buttonContainer}>
+        <button className={styles.editButton}>편집하기</button>
+        <button className={styles.shareButton}>공고 등록하기</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default StoreCard;
--- src/pages/mystore/[id].tsx
+++ src/pages/mystore/[id].tsx
@@ -1,9 +1,8 @@
 import { GetServerSideProps, NextPage } from 'next';
-import Image from 'next/image';
 import React from 'react';
 
+import StoreCard from '@/components/myStore/MyStore';
 import { GetMyStore, StoreInfo } from '@/pages/api/GetMystore';
-import locationIcon from '@/public/assets/icon/location.svg';
 
 import styles from './Mystore.module.scss';
 
@@ -17,33 +16,15 @@ const MyStore: NextPage<MyStoreProps> = ({ storeData }) => {
   }
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>내 가게</h1>
-      <div className={styles.card}>
-        <div className={styles.imageContainer}>
-          {storeData.imageUrl && (
-            <img
-              src={storeData.imageUrl}
-              alt={storeData.name}
-              className={styles.image}
-            />
-          )}
-        </div>
-        <div className={styles.content}>
-          <span className={styles.category}>식당</span>
-          <h2 className={styles.storeName}>{storeData.name}</h2>
-          <p className={styles.location}>
-            <Image src={locationIcon} alt="Location" className={styles.icon} />
-            {storeData.address1} {storeData.address2}
-          </p>
-          <p className={styles.description}>{storeData.description}</p>
-          <div className={styles.buttonContainer}>
-            <button className={styles.editButton}>편집하기</button>
-            <button className={styles.shareButton}>공고 등록하기</button>
-          </div>
-        </div>
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>내 가게</h1>
+        <StoreCard storeData={storeData} />
+
+        <h1 className={styles.title}>등록한 공고</h1>
+        <div />
       </div>
-    </div>
+    </>
   );
 };
 
