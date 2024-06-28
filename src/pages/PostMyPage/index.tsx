944448d23e65f18bed104e7448b2fe488d3bf33e
Choi <soyeongvely@gmail.com>
2024-06-28 10:56:09 +0900

    feat#61 : ul와 li 태그 및 useState로  select 기능 구현

--- src/pages/PostMyPage/index.tsx
+++ src/pages/PostMyPage/index.tsx
@@ -1,76 +1,66 @@
+import React, { useState } from 'react';
+
+import { addressOptions } from '../../utils/Options';
 import styles from './PostMyPage.module.scss';
+
+interface Option {
+  value: string;
+}
 
 interface PostMyPageProps {}
 
-const addressValue = [
-  { value: 'Jongno-gu', label: '서울시 종로구' },
-  { value: 'Jung-gu', label: '서울시 중구' },
-  { value: 'Yongsan-gu', label: '서울시 용산구' },
-  { value: 'Seongdong-gu', label: '서울시 성동구' },
-  { value: 'Gwangjin-gu', label: '서울시 광진구' },
-  { value: 'Dongdaemun-gu', label: '서울시 동대문구' },
-  { value: 'Jungnang-gu', label: '서울시 중랑구' },
-  { value: 'Seongbuk-gu', label: '서울시 성북구' },
-  { value: 'Gangbuk-gu', label: '서울시 강북구' },
-  { value: 'Dobong-gu', label: '서울시 도봉구' },
-  { value: 'Nowon-gu', label: '서울시 노원구' },
-  { value: 'Eunpyeong-gu', label: '서울시 은평구' },
-  { value: 'Seodaemun-gu', label: '서울시 서대문구' },
-  { value: 'Mapo-gu', label: '서울시 마포구' },
-  { value: 'Yangcheon-gu', label: '서울시 양천구' },
-  { value: 'Gangseo-gu', label: '서울시 강서구' },
-  { value: 'Guro-gu', label: '서울시 구로구' },
-  { value: 'Geumcheon-gu', label: '서울시 금천구' },
-  { value: 'Yeongdeungpo-gu', label: '서울시 영등포구' },
-  { value: 'Dongjak-gu', label: '서울시 동작구' },
-  { value: 'Gwanak-gu', label: '서울시 관악구' },
-  { value: 'Seocho-gu', label: '서울시 서초구' },
-  { value: 'Gangnam-gu', label: '서울시 강남구' },
-  { value: 'Songpa-gu', label: '서울시 송파구' },
-  { value: 'Gangdong-gu', label: '서울시 강동구' },
-];
+const PostMyPage: React.FC<PostMyPageProps> = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string>('선택');
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-
-
-
-function PostMyPage(props: PostMyPageProps) {
+  const handleOptionClick = (option: Option) => {
+    setSelectedOption(option.value);
+    setIsDropdownOpen(false);
+  };
 
   return (
     <main className={styles.main}>
       <div className={styles.postContainer}>
         <h1 className={styles.profileTitle}>내 프로필</h1>
         <form className={styles.formInput}>
-            <div className={styles.formarray}>
-              <div className={styles.inputSize}>
-                <label htmlFor="name" className={styles.inputFont}>이름*</label>
-                <div><input className={styles.input} type="text" id="name" name="name" placeholder="입력" /></div>
-              </div>
-            
-              <div className={styles.inputSize}>
-                <label htmlFor="tel" className={styles.inputFont}>연락처*</label>
-                <div><input className={styles.input} type="tel" id="tel" name="tel" placeholder="입력" /></div>
-              </div>
+          <div className={styles.formarray}>
+            <div className={styles.inputSize}>
+              <label htmlFor="name" className={styles.inputFont}>이름*</label>
+              <div><input className={styles.input} type="text" id="name" name="name" placeholder="입력" /></div>
+            </div>
 
-              <div className={styles.inputSize}>
-                <label htmlFor="region" className={styles.inputFont}>선호 지역</label>
-                <div>
-                  <select className={styles.selectStyle} name="region" id="region"> 선택
-                    <option value="" selected disabled hidden>선택</option>
-                    {addressValue.map(option => (
-                      <option className={styles.selectPlaceHolder} key={option.value} value={option.value}>{option.label}</option>
+            <div className={styles.inputSize}>
+              <label htmlFor="tel" className={styles.inputFont}>연락처*</label>
+              <div><input className={styles.input} type="tel" id="tel" name="tel" placeholder="입력" /></div>
+            </div>
+
+            <div className={styles.inputSize}>
+              <label htmlFor="region" className={styles.inputFont}>선호 지역</label>
+              <div>
+                <div className={styles.selectStyle} onClick={toggleDropdown}>
+                  {selectedOption}
+                  <ul className={`${styles.options} ${isDropdownOpen ? styles.show : ''}`}>
+                    {addressOptions.map((option: Option) => (
+                      <li key={option.value} className={styles.option} onClick={() => handleOptionClick(option)}>
+                        {option.value} {/* 변경: option.label 대신 option.value를 출력 */}
+                      </li>
                     ))}
-                  </select>
+                  </ul>
                 </div>
-              </div> 
-            </div>
-            
-            <div className={styles.introduceBox}>
-              <label htmlFor="intro" className={styles.inputFont}>소개</label>
-              <div><textarea className={styles.inputBoard} type="text" id="intro" name="intro" placeholder="입력" /></div>
-            </div>
+              </div>
+            </div> 
+          </div>
 
-              <button className={styles.button}><span>등록하기</span></button>
+          <div className={styles.introduceBox}>
+            <label htmlFor="intro" className={styles.inputFont}>소개</label>
+            <div><textarea className={styles.inputBoard} id="intro" name="intro" placeholder="입력" /></div>
+          </div>
+
+          <button className={styles.button}><span>등록하기</span></button>
         </form>
       </div>
     </main>
