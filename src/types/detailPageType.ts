dc1d7981dafcd5e719557890f95edfafe3c9901c
yongin6412 <105802492+yongin6412@users.noreply.github.com>
2024-06-30 13:30:21 +0900

    [#52] 상세페이지에서 버튼 클릭 시 리다이렉트와 API를 요청해서 신청 및 취소 구현하기 (#74)
    * 가게정보 API 연결
    
    * feat #22: utils 컴포넌트 사용해서 시간 및 시급증가율 변경
    
    * feat #22 : 카드 컴포넌트 API연동 테스트 및 스타일 수정
    
    * feat #22 : 상세페이지 객체구조분해할당 사용해 변수 선언
    
    * feat #36 : 처음 렌더링될 때 로컬스토리지에서 데이터를 받아와 카드컴포넌트로 Props를 내려주는 로직구현 및 카드스타일 수정
    
    * refactor #36: 타입변수 import 해서 적용해보기
    
    * 임시저장
    
    * feat #36 : 카드컴포넌트를 클릭하면 로컬 스토리지에 추가하는 코드작성
    
    * 함수명 오타 수정
    
    * 버튼 클릭 시 리다이렉트와 post 요청 구현하는중...
    
    * feat #52 : 화면 렌더링 시 세션 스토리지 데이터를 활용해 로그인여부, 프로필작성여부를 확인하고 버튼 클릭시 POST와 PUT요청 구현
    
    * feat #52: 처음 렌더링될 때 유저지원정보를 가져와 버튼상태관리완료, 새로고침되더라도 버튼의 상태값유지
    
    * feat #52 : 로그인 안되있으면 로그인페이지로 이동 프로필작성이 안되있으면 프로필 페이지로 이동

---
+++ src/types/detailPageType.ts
@@ -0,0 +1,46 @@
+export interface Props {
+  shopid: string;
+  noticeid: string;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  type: string;
+}
+
+export interface ProfileData {
+  name: string;
+  phone: string;
+  address: string;
+  bio: string;
+}
+
+export interface Application {
+  item: {
+    id: string;
+    status: 'pending' | 'accepted' | 'rejected' | 'canceled';
+    createdAt: string;
+    href: string;
+    shop: {
+      id: string;
+      name: string;
+      category: string;
+      address1: string;
+      address2: string;
+      description: string;
+      imageUrl: string;
+      originalHourlyPay: number;
+    };
+    notice: {
+      item: {
+        id: string;
+        hourlyPay: number;
+        description: string;
+        startsAt: string;
+        workhour: number;
+        closed: boolean;
+      };
+    };
+  };
+}
