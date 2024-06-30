bad6cddce9aaf21eda8b6d93d4dbdff8e7de520f
songa <thddk0121@gmail.com>
2024-06-30 21:44:58 +0900

    design #70 : 헤더와 푸터 스타일 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,21 +10,23 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>ⓒ codeit-2024</div>
-      <div className={styles.footerInfo}>
-        <Link children="Privacy Policy" href="/NotfoundPage" />
-        <Link children="FAQ" href="/NotfoundPage" />
-      </div>
-      <div className={styles.snsLinks}>
-        <Link href="/NotfoundPage">
-          <Image src={mailIcon} alt="메일 아이콘" />
-        </Link>
-        <Link href="/NotfoundPage">
-          <Image src={facebookIcon} alt="페이스북 아이콘" />
-        </Link>
-        <Link href="/NotfoundPage">
-          <Image src={intsagramIcon} alt="인스타그램 아이콘" />
-        </Link>
+      <div className={styles.footerContainer}>
+        <div className={styles.copyright}>ⓒ codeit-2024</div>
+        <div className={styles.footerInfo}>
+          <Link children="Privacy Policy" href="/NotfoundPage" />
+          <Link children="FAQ" href="/NotfoundPage" />
+        </div>
+        <div className={styles.snsLinks}>
+          <Link href="/NotfoundPage">
+            <Image src={mailIcon} alt="메일 아이콘" />
+          </Link>
+          <Link href="/NotfoundPage">
+            <Image src={facebookIcon} alt="페이스북 아이콘" />
+          </Link>
+          <Link href="/NotfoundPage">
+            <Image src={intsagramIcon} alt="인스타그램 아이콘" />
+          </Link>
+        </div>
       </div>
     </footer>
   );
