f369167f3eb734720a38b83cf317b35f40b13902
yongin <rladls6412@naver.com>
2024-07-01 02:11:42 +0900

    feat #81 : 공용 토스트 메시지 컴포넌트 제작

---
+++ src/components/common/ToastMessage.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+import styles from './ToastMessage.module.scss';
+
+interface ToastProps {
+  isOpen: boolean;
+  message: string;
+  onClose: () => void;
+}
+
+/**
+ * 사용예시
+ *
+ * <Toast
+ *  isOpen={isToastMessage}
+ *  message={'넣고 싶은 텍스트를 작성하시면 됩니다'}
+ *  onClose={() => setIsToastMessage(false)}
+ * />
+ *
+ *
+ *
+ *
+ *
+ */
+
+function Toast({ isOpen, message, onClose }: ToastProps) {
+  if (!isOpen) {
+    return;
+  }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div className={styles.messageCotainer}>
+      <div className={styles.messageBox}>{message}</div>
+    </div>
+  );
+}
+
+export default Toast;
