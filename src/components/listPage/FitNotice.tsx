97658167881aa21b76f4af4b1ce7b417483dd2dd
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-07-01 18:02:40 +0900

    feat#87: 검색 기능

--- src/components/listPage/FitNotice.tsx
+++ src/components/listPage/FitNotice.tsx
@@ -15,11 +15,17 @@ const FitNotice: React.FC<Props> = ({ initialNotices }) => {
     .slice(0, 3);
 
   return (
-    <div className={styles.carousel}>
-      <div className={styles.carouselContainer}>
-        {topNotices.map((notice) => (
-          <NoticeCard key={notice.id} notice={notice} />
-        ))}
+    <div className={styles.customContainer}>
+      <div className={styles.customSection}>
+        <h2 className={styles.title}>맞춤 공고</h2>
+        <div className={styles.fitNotice} />
+        <div className={styles.carousel}>
+          <div className={styles.carouselContainer}>
+            {topNotices.map((notice) => (
+              <NoticeCard key={notice.id} notice={notice} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
