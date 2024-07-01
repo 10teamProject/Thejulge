8abd3eaea9842b5874b02feaf64c0b7277ae6b3f
Choi <soyeongvely@gmail.com>
2024-07-01 18:19:04 +0900

    design #61 : 일부 UI 수정

--- src/pages/DetailedMyPage/index.tsx
+++ src/pages/DetailedMyPage/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import DetailCard from '../../components/detail_Profile/Detailcard';
+import styles from './DetailedMyPage.module.scss'
 
 function DetailedMyPageUpdate() {
   return (
-    <>
-      <DetailCard title="내 프로필"
-      content="내 프로필을 업데이트하고 원하는 가게에 지원해 보세요."
-      buttonText="내 프로필 수정하기" />
-    </>
+    <main className={styles.main}>
+      <div className={styles.marginBox}>
+        <DetailCard 
+          title="내 프로필"
+          content="내 프로필을 업데이트하고 원하는 가게에 지원해 보세요."
+          buttonText="내 프로필 수정하기" 
+        />
+      </div>
+    </main>
   );
 }
 
