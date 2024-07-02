9c8c59242aeb4ee275f0124747f071eeaf8aecfe
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-07-02 19:15:29 +0900

    feat#94: 케러셀 기능

--- src/components/listPage/FitNotice.tsx
+++ src/components/listPage/FitNotice.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Slider from 'react-slick';
 
 import { Notice } from '@/utils/NoticeCard/NoticesType';
 
@@ -12,20 +15,38 @@ type Props = {
 const FitNotice: React.FC<Props> = ({ initialNotices }) => {
   const topNotices = initialNotices
     .sort((a, b) => b.hourlyPay - a.hourlyPay)
-    .slice(0, 3);
+    .slice(0, 9);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000, // 5초마다 슬라이드
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false, // 좌우 이동 버튼 제거
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className={styles.customContainer}>
       <div className={styles.customSection}>
         <h2 className={styles.title}>맞춤 공고</h2>
-        <div className={styles.fitNotice} />
-        <div className={styles.carousel}>
-          <div className={styles.carouselContainer}>
-            {topNotices.map((notice) => (
-              <NoticeCard key={notice.id} notice={notice} />
-            ))}
-          </div>
-        </div>
+        <Slider {...settings} className={styles.carousel}>
+          {topNotices.map((notice) => (
+            <div key={notice.id}>
+              <NoticeCard notice={notice} />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
