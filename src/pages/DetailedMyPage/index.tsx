8ef48b0befb1d3b882755cf964d808f909a4516d
Soyeong Choi <157233323+Soyeong0926@users.noreply.github.com>
2024-07-03 10:01:06 +0900

    [#61] 내 프로필에 등록한 폼 데이터 보내기 (#89)
    * feat#61 : ul와 li 태그 및 useState로  select 기능 구현
    
    * feat #61 : 선호 지역 select box UI 구현 완성
    
    * design #67: UI 일부 수정
    
    * feat #61 : 변경사항
    
    * feat #61 : 사용자 데이터 값을 put 요청으로 보내기 및 업데이트 성공 시 모달창 띄우기
    
    * feat #61 : 모달창 확인 버튼 클릭 시 다음 페이지로 리다이렉트
    
    * design #61 : 리다이렉트 페이지 UI 구현 중
    
    * design #61 : 일부 UI 수정
    
    * feat #61 : 브랜치 변경
    
    ---------
    
    Co-authored-by: HANSANGWOO <99471821+Han-wo@users.noreply.github.com>

--- src/pages/DetailedMyPage/index.tsx
+++ src/pages/DetailedMyPage/index.tsx
@@ -1,11 +1,69 @@
+import Image from 'next/image';
 import React from 'react';
 
-import DetailCard from '../../components/detail_Profile/Detailcard';
+import location from '@/public/assets/images/loacation.svg'
+import phoneIcon from '@/public/assets/images/phone.svg'
 
-function DetailedMyPage() {
+import DetailCard from '../../components/detail_Profile/Detailcard';
+import styles from './MyPage.module.scss'
+
+function MyPage() {
   return (
-    <DetailCard />
+    <main className={styles.main}>
+      <div className={styles.marginBox}>
+        <DetailCard 
+          title="내 프로필"
+          content="내 프로필을 업데이트하고 원하는 가게에 지원해 보세요."
+          buttonText="내 프로필 수정하기" 
+        />
+      </div>
+      <div className='renderingPage'>
+        <div className={styles.profileWrap}>
+          <div className={styles.informationWrap}>
+            <h1 className={styles.profileTitle}>내 프로필</h1>
+            <div className={styles.userWrap}>
+              <div className={styles.userFlex}>
+                <div className={styles.titleWrap}>
+                <h4 className={styles.nameProfile}>이름</h4>
+                <p className={styles.userName}>김승우</p>
+                
+                <div className={styles.informationFlex}>
+                  <Image
+                    src={phoneIcon}
+                    alt="폰 아이콘"
+                    width={20} 
+                    height={20} 
+                  />
+                  <p className={styles.information}>010-1234-5678</p>
+                </div>
+                <div className={styles.informationFlex}>
+                  <Image
+                    src={location}
+                    alt="폰 아이콘"
+                    width={20} 
+                    height={20} 
+                  />
+                  <p className={styles.information}>선호 지역: {/*지역 데이터*/}</p>
+                </div>
+                  <p className={`${styles.information} ${styles.bio}`}>열심히 일 하겠습니다.</p>
+                </div>
+                <div>
+                  <button className={styles.button}>편집하기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.backgroundColor}>
+        <DetailCard title="신청 내역"
+          content="아직 신청 내역이 없어요."
+          buttonText="공고 보러가기"
+        />  
+        </div>
+    </div>
+    </main>
   );
 }
 
-export default DetailedMyPage;
+export default MyPage;
