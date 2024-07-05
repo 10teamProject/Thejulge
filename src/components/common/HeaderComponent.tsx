8065ec0bb092db1ad9a0ccc43f48298eb89cd4ad
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-07-05 09:13:24 +0900

    design #112 : 헤더에 이미지 추가

--- src/components/common/HeaderComponent.tsx
+++ src/components/common/HeaderComponent.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -5,6 +6,7 @@ import Search from '@/components/listPage/Search';
 import UserNotification from '@/components/userNofication/UserNofication';
 import { useAuth } from '@/contexts/AuthProvider';
 import { GetUserInfo } from '@/pages/api/GetUserInfo';
+import coinlogo from '@/public/assets/images/coinicons.png';
 
 import styles from './Header.module.scss';
 
@@ -32,6 +34,7 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
+        <Image src={coinlogo} alt="로고" width={40} height={40} />
         <Link href="/listPage" className={styles.logo}>
           Pay Plus+
         </Link>
